1dff89d2ebbd227973d956b9699f4fbeb577b54d
Keyur <savalia.keyur1091@gmail.com>
2026-02-20 14:32:20 -0800

    Initial Commit

---
+++ BalanceZero/BalanceZeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalanceZeroApp.swift
+//  BalanceZero
+//
+//  Created by Keyur Savalia on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct BalanceZeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
