2dfdb796ddf77383a3566b400a929fa8fe7f24af
Keyur <savalia.keyur1091@gmail.com>
2026-02-20 18:48:43 -0800

    fix: conformed MatchQuality, SelectedItem and OptimizationResult to be Hashable

--- BalanceZero/Model/OptimizationResult.swift
+++ BalanceZero/Model/OptimizationResult.swift
@@ -1,12 +1,12 @@
 import Foundation
 
-enum MatchQuality: Equatable {
+enum MatchQuality: Equatable, Hashable {
     case perfect                      // remainder == 0
     case partial(remainingCents: Int) // best we can do & remainder > 0
     case noSolution                   // no item fits at all
 }
 
-struct SelectedItem: Equatable {
+struct SelectedItem: Equatable, Hashable {
     let item: ShoppingItem
     let quantity: Int
     
@@ -15,7 +15,7 @@ struct SelectedItem: Equatable {
     }
 }
 
-struct OptimizationResult: Equatable {
+struct OptimizationResult: Equatable, Hashable {
     let balanceInCents: Int
     let selectedItems: [SelectedItem]
     let matchQuality: MatchQuality
