5f69fa9b00fe0b6babd714d1f0230ad4ac0bde29
Keyur <savalia.keyur1091@gmail.com>
2026-02-27 14:27:19 -0800

    add: mandatoryQuantity to ShoppingItem for user-specified required items

--- BalanceZero/Model/ShoppingItem.swift
+++ BalanceZero/Model/ShoppingItem.swift
@@ -5,11 +5,14 @@ struct ShoppingItem: Identifiable, Hashable, Equatable {
     let id: UUID
     var name: String
     var priceInCents: Int // not using doubles to avoid floating-point drift
+    /// Quantity user wants to include. 0 = optional; 1+ = mandatory in optimization.
+    var mandatoryQuantity: Int
     
-    init(id: UUID = UUID(), name: String, priceInCents: Int) {
+    init(id: UUID = UUID(), name: String, priceInCents: Int, mandatoryQuantity: Int = 0) {
         self.id = id
         self.name = name
         self.priceInCents = priceInCents
+        self.mandatoryQuantity = mandatoryQuantity
     }
     
     var priceForDisplay: Decimal {
